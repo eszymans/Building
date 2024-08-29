d7386f308e0a7b56b17e761ee159c070114e278b
eszymans <edytakociak@gmail.com>
2024-08-29 21:14:49 +0200

    New Work

--- workshop/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/pobi/start/szymanska/workshop
-# Build directory: /mnt/c/pobi/start/szymanska/workshop/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("library")
-subdirs("program")
--- workshop/build/library/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/pobi/start/szymanska/workshop/library
-# Build directory: /mnt/c/pobi/start/szymanska/workshop/build/library
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LibraryTest "LibraryTester" "--report_level=detailed" "--log_level=all" "--color_output=yes")
-set_tests_properties(LibraryTest PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/pobi/start/szymanska/workshop/library/CMakeLists.txt;75;add_test;/mnt/c/pobi/start/szymanska/workshop/library/CMakeLists.txt;0;")
